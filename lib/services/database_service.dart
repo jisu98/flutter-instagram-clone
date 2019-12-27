b896f90625c841e08b0cd179de0da4c35f4e0c25
jisu <imsrhs60l@korea.ac.kr>
2019-12-27 15:06:19 +0900

    ~chapter 7(create posts, image cropper)

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:instagram_clone/models/post_model.dart';
 import 'package:instagram_clone/models/user_model.dart';
 import 'package:instagram_clone/utilities/constants.dart';
 
@@ -17,5 +18,15 @@ class DatabaseService {
         .where('name', isLessThanOrEqualTo: name + '\uf8ff')
         .getDocuments();
     return users;
+  }
+
+  static void createPost(Post post) {
+    postsRef.document(post.authorId).collection('usersPost').add({
+      'imageUrl': post.imageUrl,
+      'caption': post.caption,
+      'likes': post.likes,
+      'authorId': post.authorId,
+      'timestamp': post.timestamp,
+    });
   }
 }
